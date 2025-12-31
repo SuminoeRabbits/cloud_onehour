d67c73be04085fde3a7c07725a822d02e3638685
Suminoe Rappid <zima@mtj.biglobe.ne.jp>
2025-12-31 18:59:02 +0900

    update

--- pts_regression.py
+++ pts_regression.py
@@ -6,10 +6,28 @@
 # ./script/run_pts_benchmark.py <testname> <number> 2>&1 \
 # | tee -a ./reports/<machine>/<test categoly>/<testname>.log
 #
-# を生成します。なお./reports/<machine>/<test categoly>/は
+# を生成します。なおrun_pts_benchmark.py は
 # run_pts_benchmark.py が各テスト毎にログを出すディレクトリです。
 # 
 # 1. Test 生成について
+# test_suite.jsonで "enabled": true, のテストのみを有効とします。 
+# "test_category"の層で"enabled": false, の場合、
+# その配下のテストは全て無効となります。
 # 2. <testname>の決定
-# 3. <>
+# 有効なテストの"items"配下の"pts/<testname>"で開始するフィールドを
+# <testname>とします。
+# 3. <number>の決定
+# <number>はtest_suite.jsonの有効なテストの"items"配下の
+# "pts/<testname>"配下の
+# "THFix_in_compile", "THChange_at_runtime", "TH_scaling"
+# で決定されます。決定方法は以下の順番です。
+#  if  "THFix_in_compile"==true
+#       <number>=HardwareのCPU数、ie `nproc`
+#  else
+#    if "THChange_at_runtime"==true
+#       <number> は何も指定してはいけない。
+#    else
+#       <number>=1
+# 4. reports ディレクトリの決定
+# 5. 実行コマンドの発行
 # 
