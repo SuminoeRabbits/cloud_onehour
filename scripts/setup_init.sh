069b6f58efe388c6e26a51119167fc92ca7dca02
Suminoe Rappid <zima@mtj.biglobe.ne.jp>
2026-01-03 18:46:29 +0900

    update

--- scripts/setup_init.sh
+++ scripts/setup_init.sh
@@ -9,4 +9,5 @@ sudo apt-get -y install bc
 sudo apt-get -y install uuid-dev libxml2-dev
 # install cpupower
 sudo apt-get -y install linux-tools-common linux-tools-$(uname -r)
-sudo apt-get -y install sysstat htop
+sudo apt-get -y install sysstat htop aria2 curl
+sudo apt-get -y install flex bison libssl-dev libelf-dev
