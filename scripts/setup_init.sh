781a32073eefbdac2e3698bd86d97d19d88b38fa
Suminoe Rappid <zima@mtj.biglobe.ne.jp>
2026-01-19 01:12:59 +0900

    bug fix

--- scripts/setup_init.sh
+++ scripts/setup_init.sh
@@ -12,6 +12,7 @@ sudo apt-get -y install linux-tools-common linux-tools-$(uname -r)
 sudo apt-get -y install sysstat htop aria2 curl
 sudo apt-get -y install flex bison libssl-dev libelf-dev libevent-dev
 sudo apt-get install -y python3-tabulate libexpat1-dev
+sudo apt-get install -y cl-ppcre libpcre3-dev
 # 1. Architecture Detection
 ARCH=$(uname -m)
 OS_ID=$(lsb_release -is)
