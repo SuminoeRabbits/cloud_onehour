324cdaa17abe5adba321bb43bb5088dc986f935d
Suminoe Rappid <zima@mtj.biglobe.ne.jp>
2026-01-31 14:10:22 +0900

    update

--- scripts/setup_init.sh
+++ scripts/setup_init.sh
@@ -6,7 +6,7 @@ set -e
 # install various tools depends on your local needs.
 sudo apt-get -y update
 sudo apt-get -y install bc
-sudo apt-get -y install uuid-dev libxml2-dev pkg-config
+sudo apt-get -y install uuid-dev libxml2-dev pkg-config libcurl4-openssl-dev libjansson-dev
 # install cpupower
 sudo apt-get -y install linux-tools-common linux-tools-$(uname -r)
 sudo apt-get -y install sysstat htop aria2 curl
