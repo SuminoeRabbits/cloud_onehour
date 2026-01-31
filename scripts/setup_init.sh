9bf89da6af887c2ede9191e83fd4eb7fbeefa8b5
Suminoe Rappid <zima@mtj.biglobe.ne.jp>
2026-01-31 12:11:00 +0900

    update

--- scripts/setup_init.sh
+++ scripts/setup_init.sh
@@ -6,7 +6,7 @@ set -e
 # install various tools depends on your local needs.
 sudo apt-get -y update
 sudo apt-get -y install bc
-sudo apt-get -y install uuid-dev libxml2-dev
+sudo apt-get -y install uuid-dev libxml2-dev pkg-config
 # install cpupower
 sudo apt-get -y install linux-tools-common linux-tools-$(uname -r)
 sudo apt-get -y install sysstat htop aria2 curl
