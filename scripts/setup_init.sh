67eb1b3932d661a83a0cecfe1d0cea5ad73df10f
Suminoe Rappid <zima@mtj.biglobe.ne.jp>
2026-02-04 23:18:59 +0900

    update

--- scripts/setup_init.sh
+++ scripts/setup_init.sh
@@ -13,7 +13,6 @@ sudo apt-get -y install sysstat htop aria2 curl
 sudo apt-get -y install flex bison libssl-dev libelf-dev libevent-dev
 sudo apt-get install -y python3-tabulate libexpat1-dev
 sudo apt-get install -y cl-ppcre libpcre3-dev
-sudo apt-get install -y python3-pip
 sudo apt-get -y install p7zip-full
 sudo apt-get install -y libc6-dev
 
